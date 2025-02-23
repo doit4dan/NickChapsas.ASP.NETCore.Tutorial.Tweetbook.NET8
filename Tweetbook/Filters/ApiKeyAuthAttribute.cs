fabc8abfafd560bb95ab315efab8d5b4b29e40fb
Daniel Carson <DCarson@apus.edu>
2025-02-22 22:53:19 -0500

    Setting up ApiKey-based Authentication | ASP.NET Core 5 REST API Tutorial 24

---
+++ Tweetbook/Filters/ApiKeyAuthAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Tweetbook.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)] // attribute can only be used on class and method
+    public class ApiKeyAuthAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string ApiKeyHeaderName = "ApiKey";
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            // e.g. You could also get Api Key from query string
+            // context.HttpContext.Request.Query["ApiKey"]
+
+            // before it gets to controller
+            if(!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = configuration.GetValue<string>("ApiKey");
+
+            if(!apiKey.Equals(potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            await next(); // calls controller
+
+            // after it passes controller
+        }
+    }
+}
